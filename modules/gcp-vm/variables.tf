06f700a2551c9dcc5f3b579e40ef0116eb5a915d
anisha2902 <154617956+anisha2902@users.noreply.github.com>
2026-01-29 14:19:30 +0530

    Update variables.tf

--- modules/gcp-vm/variables.tf
+++ modules/gcp-vm/variables.tf
@@ -1,10 +1,8 @@
 variable "zone" {
   description = "The GCP zone to deploy resources in"
   type        = string
-  default     = "us-east1-b"
 }
 
 variable "machine_type" {
   type        = string
-  default     = "e2-small"
 }
